a5abb99d28f376efbff6fc562620d4a4316568cc
qtplatypus <qtplatypus@gmail.com>
2017-11-20 09:21:51 +1000

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -1,6 +1,24 @@
 define("power", [], function () {
-    
-    return {}
+    var power_grid = {
+	    "charge" : 0
+	    ,"add_power" : function (amount, source) {
+		    this.charge += amount;
+		    this.source[source][0] += amount;
+	    }
+	    ,"remove_power" : function (amount, sink) {
+		    if (this.charge >= amount) {
+          		    this.charge -= amount;
+			    this.sink[sink][0] += amount;
+			    return amount;
+		    }
+		    else {
+			    var diff = amount - this.charge;
+			    this.charge = 0;
+			    this.sink[sink][0] += diff;
+			    return diff;
+		    }
+	    }
+    }
 });
 
 	

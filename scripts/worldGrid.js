10f9bb8ad2e98dedbb5da8d1cd5ef5721af94f33
qtplatypus <qtplatypus@gmail.com>
2017-11-24 18:35:37 +1000

    Update worldGrid.js

--- scripts/worldGrid.js
+++ scripts/worldGrid.js
@@ -2,7 +2,6 @@ function isContainer (x,y) {
     return ((x % 2) != (y % 2));
 }
 
-/*
 define("worldGrid", [], function () {
     return function () {
         var grid = [];
@@ -74,4 +73,4 @@ define("worldGrid", [], function () {
     // Display object
     
 });
-*/
+

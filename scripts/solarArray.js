665e0cf31488255e83dca0feb248df299df9a727
qtplatypus <qtplatypus@gmail.com>
2017-11-28 17:51:52 +1000

    Update solarArray.js

--- scripts/solarArray.js
+++ scripts/solarArray.js
@@ -1,6 +1,18 @@
 define("solarArray", [], function () {
    return function () {
-       return {
-       }
+      
+      var solarArray = {};
+      
+      window.setInterval(function () {
+         if (solarArray.powerGrid == undefined) {
+            return;
+         }
+         
+         var power = (1 << 15) / length (solarArray.powerGrid);
+         
+         solarArray.powerGrid.foreach(function (grid) {
+            grid.produce(power, "solarArray");
+         }
+	    }, 1e3);
     }
 }
